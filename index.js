e37abf8fcf841ef0ed78ca85d051b3c4dba87ff5
Cmango005 <mh410676@gmail.com>
2023-12-13 13:25:15 +0600

    user can delete items from server untill status become on the way commit

--- index.js
+++ index.js
@@ -157,6 +157,12 @@ async function run() {
             const result = await orderCollection.updateOne(filter, updatedDoc);
             res.send(result);
         })
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await orderCollection.deleteOne(query);
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
