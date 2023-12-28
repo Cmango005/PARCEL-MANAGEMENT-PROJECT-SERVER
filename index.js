cc12f19fa5bd240cb4221f9a26c5d1073ea4415c
Cmango005 <mh410676@gmail.com>
2023-12-28 19:09:01 +0600

    payment by card server commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -164,6 +165,22 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result = await orderCollection.deleteOne(query);
             res.send(result);
+        })
+        app.post('/create-payment-intent', async(req, res)=>{
+           const {price} = req.body;
+           const amount = parseInt(price*100);
+           console.log(amount);
+           const paymentIntent = await stripe.paymentIntents.create({
+            amount : amount,
+            currency: "usd",
+            payment_method_types: [
+                "card"
+            ]
+           });
+
+           res.send({
+            clientSecret: paymentIntent.client_secret
+           })
         })
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
